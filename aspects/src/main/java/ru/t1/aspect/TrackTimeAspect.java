14edf14e2a0ea61ce14032a4ca0ac06c0a616aed
Trushanina Maria <ext_mtrushanina@rencredit.ru>
2024-04-08 20:39:43 +0300

    Initial commit

---
+++ aspects/src/main/java/ru/t1/aspect/TrackTimeAspect.java
@@ -0,0 +1,23 @@
+package ru.t1.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class TrackTimeAspect {
+
+    @Around("@annotation(TrackTime)")
+    public Object trackTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        log.info("Starting tracking time at {}", startTime);
+        Object proceed = joinPoint.proceed();
+        long endTime = System.currentTimeMillis();
+        log.info("Ending tracking time at {}", endTime);
+        return proceed;
+    }
+}
---
+++ aspects/src/main/java/ru/t1/aspect/TrackTimeAsyncAspect.java
@@ -0,0 +1,36 @@
+package ru.t1.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+
+@Aspect
+@Component
+@Slf4j
+public class TrackTimeAsyncAspect {
+
+    @Around("@annotation(TrackTimeAsync)")
+    public Object trackTimeAsync(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        Executors.newFixedThreadPool(2).execute(() -> {
+            CompletableFuture.runAsync(() -> {
+                log.info("Started async calculations at {}", startTime);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    log.error("Ran into error, reason: {}", e.getMessage(), e);
+                }
+            });
+        });
+        long endTime = System.currentTimeMillis();
+        log.info("Finished async calculations at {}", endTime);
+        log.info("The time for async calculations is {} milliseconds", endTime - startTime);
+        return proceed;
+    }
+}
